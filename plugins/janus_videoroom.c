9e816d2d88a102180fb7da680be238c170d79258
PavelAtCarbyne <pavel@carbyne911.com>
2020-06-29 09:27:46 +0000

    remove room disabled , when publisher disconnected

--- plugins/janus_videoroom.c
+++ plugins/janus_videoroom.c
@@ -5824,15 +5824,15 @@ void janus_videoroom_hangup_media(janus_plugin_session *handle) {
        	                janus_videoroom *videoroom =  g_hash_table_lookup(rooms,string_ids ? (gpointer)room_id_str : (gpointer)&room_id);
                	        if(videoroom) {
                        	        /* Remove room, but add a reference until we're done */
-                             	if(!string_ids) {
+                             /*	if(!string_ids) {
                                        	JANUS_LOG(LOG_INFO, "PUBLISHER: Remove room %"SCNu64" permanently \n",room_id);
                        		}
                        		else {
                          	 	JANUS_LOG(LOG_INFO, "PUBLISHER: Remove room %s permanently \n", room_id_str);
-                        	}
+                        	}*/
                         	janus_mutex_lock(&videoroom->mutex);
                         	janus_refcount_increase(&videoroom->ref);
-                        	g_hash_table_remove(rooms, string_ids ? (gpointer)room_id_str : (gpointer)&room_id);
+                        	//g_hash_table_remove(rooms, string_ids ? (gpointer)room_id_str : (gpointer)&room_id);
                         	g_free(room_id_str);
 
           			/*CARBYNE-RF FREE */
